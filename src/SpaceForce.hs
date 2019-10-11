000141bea27f529a86aa41fa755cf851b530f20e
Mike <super.mvk@yandex.ru>
2019-10-12 00:38:32 +0300

    Animate simple circle

--- src/SpaceForce.hs
+++ src/SpaceForce.hs
@@ -1,10 +1,28 @@
 module SpaceForce where
 
 import Graphics.Gloss
+import Data.Fixed (mod')
 
-window :: IO ()
-window = display (InWindow "My Nice Window" (200,200) (10,10)) white (Circle 80)
+-- | Default for displaying window: Name Size Position
+defDisplay :: Display
+defDisplay = (InWindow "My Awesome Window" (1280, 720) (10, 10))
+
+-- | Default color for background
+defBackClr :: Color
+defBackClr = white
+
+simpleDraw :: IO ()
+simpleDraw = display defDisplay defBackClr (Circle 80)
+
+myCircle :: Picture
+myCircle = Circle 80
+
+motion :: Float -> Picture
+motion dt = translate ((dt `mod'` 1)*20) 0 myCircle
+
+simpleAnim :: IO ()
+simpleAnim = animate defDisplay defBackClr motion
 
 run :: IO ()
-run = window
+run = simpleAnim
 -- run = putStrLn "Hello world, Gloss!"
