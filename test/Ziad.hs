e75d299d03d33f0c91597e515fa36159b769c23a
Silver <ziadxkabakibi@hotmail.com>
2019-10-13 00:30:21 +0300

    added code for swinging pendulum (well it's actually just a line LOL)

--- test/Ziad.hs
+++ test/Ziad.hs
@@ -2,6 +2,7 @@
 -- https://mmhaskell.com/blog/2019/3/25/making-a-glossy-game-part-1
 
 import Graphics.Gloss
+import Graphics.Gloss.Data.ViewPort
 
 window::Display
 window = InWindow "Nice Window" (200,200) (10,10)
@@ -23,5 +24,30 @@ animationFunc time = Circle (2 * time)
 animatedCircle:: IO ()
 animatedCircle = animate window background animationFunc
 
+-- The simulation for a swinging pendulum
+type Model = (Float, Float)
+
+simulation :: IO ()
+simulation = simulate
+ window
+  background
+  simulationRate
+  initialModel
+  drawingFunc
+  updateFunc
+  where
+    simulationRate :: Int
+    simulationRate = 20
+
+    initialModel :: Model
+    initialModel = (0,0)
+
+    drawingFunc :: Model -> Picture
+    drawingFunc (theta, dtheta) = Line [(0,0), (200 * cos theta, 200 * sin theta)]
+
+    updateFunc :: ViewPort -> Float -> Model -> Model
+    updateFunc _ dt (theta, dtheta) = (theta + dt * dtheta, dtheta - dt * (cos theta))
+
+
 main::IO()
-main = animatedCircle
+main = simulation
