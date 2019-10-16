fa685f9e1cae7077ff11dc363d2cdd6e4f45cbba
Silver <ziadxkabakibi@hotmail.com>
2019-10-16 14:37:26 +0300

    rearranged files

--- test/Main.hs
+++
@@ -1,9 +0,0 @@
-{-# OPTIONS_GHC -fdefer-typed-holes #-}
-{-# LANGUAGE DeriveFoldable    #-}
-{-# LANGUAGE DeriveFunctor     #-}
-{-# LANGUAGE EmptyDataDeriving #-}
-
-module Main where
-
-main :: IO ()
-main = putStrLn "This code is working !\n"
